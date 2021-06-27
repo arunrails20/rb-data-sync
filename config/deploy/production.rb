8763d628a1ae33a464ef96f8fb2443a8f3bd206d
arunrails20 <arunrails20@gmail.com>
2021-06-27 22:16:12 +0530

    prod cap details

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,13 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+
+# ~/dev/trackerr/config/deploy/production.rb 
+
+server '18.119.11.35', user: 'ubuntu', roles: %w{web app db}
+set :ssh_options, { 
+  forward_agent: true, 
+  auth_methods: %w[publickey],
+  keys: %w[~/server-keys/efrl-rb-data-sync.pem]
+}
